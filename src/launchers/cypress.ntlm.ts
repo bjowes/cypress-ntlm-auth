24924eda20dc17a80f4443f3fe979cb10700d877
Björn Weström <bjowes@gmail.com>
2020-09-20 00:13:14 +0200

    improved cli argument parsing

--- src/launchers/cypress.ntlm.ts
+++ src/launchers/cypress.ntlm.ts
@@ -2,24 +2,38 @@
 
 import { checkCypressIsInstalled, run, open, debug } from "../index";
 
-async function prepareOptions() {
+async function prepareOptions(args: string[]) {
   const cypress = require("cypress");
-  let cliArguments = process.argv.slice(3);
+  let cliArguments = args.slice(1);
   cliArguments.unshift("run");
   cliArguments.unshift("cypress");
   return await cypress.cli.parseRunArguments(cliArguments);
 }
 
+function getArgsAfterCypressNtlm() {
+  const cypressNtlmIndex = process.argv.findIndex(
+    (t) => t === "cypress-ntlm" || t.endsWith("node_modules/.bin/cypress-ntlm")
+  );
+  if (cypressNtlmIndex === -1) {
+    debug.log(process.argv);
+    throw new Error("Cannot parse command line arguments");
+  }
+  return process.argv.slice(cypressNtlmIndex + 1);
+}
+
 async function execute() {
   checkCypressIsInstalled();
-  if (process.argv[2] === "open") {
-    const options = await prepareOptions();
+  const args = getArgsAfterCypressNtlm();
+  if (args[0] === "open") {
+    const options = await prepareOptions(args);
     const results = await open(options);
-  } else if (process.argv[2] === "run") {
-    const options = await prepareOptions();
+  } else if (args[0] === "run") {
+    const options = await prepareOptions(args);
     const results = await run(options);
   } else {
-    throw new Error("Unsupported command");
+    throw new Error(
+      "Unsupported command, use cypress-ntlm open or cypress-ntlm run."
+    );
   }
 }
 
