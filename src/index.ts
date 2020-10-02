954b7ec7ed8a9ea30b5b993e37875f74ef6755f5
Björn Weström <bjowes@gmail.com>
2020-10-02 21:38:05 +0200

    docs for module api

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,34 @@ const container = new DependencyInjection();
 const startup = container.get<IStartup>(TYPES.IStartup);
 const debug = container.get<IDebugLogger>(TYPES.IDebugLogger);
 
+/**
+ * Starts Cypress in headless mode with NTLM plugin
+ * @param options An options object as defined by https://docs.cypress.io/guides/guides/module-api.html#Options
+ */
 export async function run(options: any) {
   return await startup.run(options);
 }
 
+/**
+ * Starts Cypress in headed mode with NTLM plugin
+ * @param options An options object as defined by https://docs.cypress.io/guides/guides/module-api.html#Options-1
+ */
 export async function open(options: any) {
   return await startup.open(options);
 }
 
+/**
+ * Converts command line arguments to Cypress mode ('run' or 'open')
+ * @param args command line arguments
+ */
 export function argumentsToCypressMode(args: string[]) {
   return startup.argumentsToCypressMode(args);
 }
 
+/**
+ * Converts command line arguments to a Cypress options object.
+ * @param args command line arguments
+ */
 export async function argumentsToOptions(args: string[]) {
   return await startup.prepareOptions(args);
 }
