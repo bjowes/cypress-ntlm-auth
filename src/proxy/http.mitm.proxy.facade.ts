6156519b9d5a84ae24e4e77e5d49cad67effa3e3
Björn Weström <bjowes@gmail.com>
2022-03-01 22:01:09 +0100

    enforce 127.0.0.1

--- src/proxy/http.mitm.proxy.facade.ts
+++ src/proxy/http.mitm.proxy.facade.ts
@@ -18,14 +18,17 @@ export class HttpMitmProxyFacade implements IHttpMitmProxyFacade {
 
   listen(port: number): Promise<string> {
     return new Promise<string>((resolve, reject) =>
-      this._ntlmProxy.listen({ host: "localhost", port: port, keepAlive: true, forceSNI: false }, (err: Error) => {
-        if (err) {
-          return reject(err);
+      this._ntlmProxy.listen(
+        { host: "127.0.0.1", port: port, keepAlive: true, forceSNI: false },
+        (err: Error) => {
+          if (err) {
+            return reject(err);
+          }
+          this._ntlmProxyListening = true;
+          const url = "http://127.0.0.1:" + this._ntlmProxy.httpPort;
+          resolve(url);
         }
-        this._ntlmProxyListening = true;
-        const url = "http://127.0.0.1:" + this._ntlmProxy.httpPort;
-        resolve(url);
-      })
+      )
     );
   }
 
